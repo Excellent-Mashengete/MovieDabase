0c31554d19b6d85162c464a1bcfe9af564f1102e
Excellent <excellent.mashengete@dadevs.co.za>
2022-08-04 15:23:08 +0200

    added authentication

--- frontend/src/app/component/auth/Service/authentication.service.ts
+++ frontend/src/app/component/auth/Service/authentication.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Login } from 'src/app/interface/login';
+import { Register } from 'src/app/interface/register';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+const token = localStorage.getItem('access_token');
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'token': `${token}` })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient,private router: Router) { }
 
-  constructor() { }
+  login(users : Login): Observable<any> {
+    return this.http.post(`${this.baseUrl}login`, users)
+  }
+
+  register(users : Register) {
+    return this.http.post(`${this.baseUrl}register`, users);
+  }
+  
+  getToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  get isLoggedIn(): boolean {
+    let authToken = localStorage.getItem('access_token');
+    return authToken !== null ? true : false;
+  }
+  
+  doLogout() {
+    let removeToken = localStorage.removeItem('access_token');
+    if (removeToken == null) {
+      this.router.navigate(['products']);
+    }
+  }
+  
+  getUserProfile(){
+    return this.http.get(`${this.baseUrl}profile`,httpOptions)
+  }
 }
--- frontend/src/app/component/public/navbar/navbar.component.ts
+++ frontend/src/app/component/public/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../auth/Service/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthenticationService) { }
 
   ngOnInit(): void {
   }
@@ -15,6 +16,6 @@ export class NavbarComponent implements OnInit {
   user = "name"
 
   Logout(){
-    
+    this.auth.doLogout
   }
 }
